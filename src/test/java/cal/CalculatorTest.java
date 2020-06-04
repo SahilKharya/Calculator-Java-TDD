d1f8b0b6a75ee712ead3af0cb5315d7f5e916cd2
“SahilKharya” <kharyasahil@gmail.com>
2020-06-04 14:56:34 +0530

    Test for adding unknown numbers

--- src/test/java/cal/CalculatorTest.java
+++ src/test/java/cal/CalculatorTest.java
@@ -14,16 +14,23 @@ class CalculatorTest {
 	}
 	
 	@Test
-	public void  addSingleNumber() {
+	public void addSingleNumber() {
 		assertEquals(1, calculator.Add("1"), "Sum of one value is that value only.");
 		assertEquals(3, calculator.Add("3"), "Sum of one value is that value only.");
 		assertEquals(17, calculator.Add("17"), "Sum of one value is that value only.");
 	}
 	
 	@Test
-	public void  addTwoNumbersCommaSeperated() {
+	public void addTwoNumbersCommaSeperated() {
 		assertEquals(3, calculator.Add("1,2"), "Wrong result for adding two values.");
 		assertEquals(10, calculator.Add("3,7"), "Wrong result for adding two values.");
 		assertEquals(29, calculator.Add("12,17"), "Wrong result for adding two values.");
+	}
+	
+	@Test
+	public void addMultipleNumbers() {
+		assertEquals(3, calculator.Add("1,1,1"), "Wrong result for adding values.");
+		assertEquals(40, calculator.Add("3,7,10,5,15"), "Wrong result for adding values.");
+		assertEquals(83, calculator.Add("12,17,51,3"), "Wrong result for adding values.");
 	}
 }
