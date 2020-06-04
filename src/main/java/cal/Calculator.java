bdf1c2e90faa1af517e2f90418b88f8d4244a902
“SahilKharya” <kharyasahil@gmail.com>
2020-06-05 00:11:26 +0530

    throw negatives not allowed exception code

--- src/main/java/cal/Calculator.java
+++ src/main/java/cal/Calculator.java
@@ -2,12 +2,31 @@ package cal;
 
 public class Calculator {
 
+	private String delimiter = ",";
+
 	public int Add(String value) {
-		String delimiter;
+
 		if (value.isEmpty()) {
 			// add null string
 			return 0;
 		}
+		if (value.contains("-")) {
+			String num = "";
+			// add unknown numbers separated by delimiter (comma / new line)
+			String[] numbers = value.split(delimiter);
+			for (int i = 0; i < numbers.length; i++) {
+				int a = Integer.parseInt(numbers[i]);
+				if (a < 0) {
+					num = num + numbers[i] + ",";
+				}
+			}
+			num = num.substring(0, num.length() - 1);
+			throw new RuntimeException("negatives not allowed: " + num);
+		}
+		return sumValue(value);
+	}
+
+	private int sumValue(String value) {
 		if (value.startsWith("//")) {
 			String[] exp = value.split("\n", 2);
 			delimiter = exp[0].substring(2);
@@ -16,7 +35,6 @@ public class Calculator {
 			delimiter = ",|\n";
 		}
 		return sumOfDelimiterSeparatedNumbers(value, delimiter);
-
 	}
 
 	private int sumOfDelimiterSeparatedNumbers(String value, String delimiter) {
