a3461d7d019a72189e0784d4758730928d30d34f
“SahilKharya” <kharyasahil@gmail.com>
2020-06-04 14:48:42 +0530

    Two Numbers addition code

--- src/main/java/cal/Calculator.java
+++ src/main/java/cal/Calculator.java
@@ -3,14 +3,15 @@ package cal;
 public class Calculator {
 
 	public int Add(String value) {
-		if(value.isEmpty()) {
+		if (value.isEmpty()) { // add null string
 			return 0;
+		} else if (value.contains(",")) { // for two numbers seperated by comma
+			String[] numbers = value.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
 		} else {
 			return Integer.parseInt(value);
 		}
-		
+
 	}
-	
-	
 
 }
