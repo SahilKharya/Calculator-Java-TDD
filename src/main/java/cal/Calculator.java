3eaae949a19e0557532892558e1106b9b2ce412f
“SahilKharya” <kharyasahil@gmail.com>
2020-06-04 15:14:12 +0530

    Refactor

--- src/main/java/cal/Calculator.java
+++ src/main/java/cal/Calculator.java
@@ -7,19 +7,23 @@ public class Calculator {
 			// add null string
 			return 0;
 		} else if (value.contains(",")) {
-			int sum = 0;
-			// add unknown numbers separated by comma
-			String[] numbers = value.split(",");
-			for (int i = 0; i < numbers.length; i++) {
-				int a = Integer.parseInt(numbers[i]);
-				sum += a;
-			}
-			return sum;
+			return sumOfCommaSeparatedNumbers(value);
 		} else {
 			// sum of single number
 			return Integer.parseInt(value);
 		}
 
+	}
+
+	private int sumOfCommaSeparatedNumbers(String value) {
+		int sum = 0;
+		// add unknown numbers separated by comma
+		String[] numbers = value.split(",");
+		for (int i = 0; i < numbers.length; i++) {
+			int a = Integer.parseInt(numbers[i]);
+			sum += a;
+		}
+		return sum;
 	}
 
 }
